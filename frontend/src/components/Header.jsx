768dc5c0ef7a6f28117bc4f4ff6cdf55dc3de55d
devanhdq <dev.anhdq@gmail.com>
2024-07-08 14:57:11 +0700

    create component header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import {
+  FaCodeCompare,
+  FaHeart,
+  FaRegUser,
+  FaCartFlatbed,
+} from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <div className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free shipping for standard order over $100 & Free return
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+84888888">
+                  0122-122-123
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">E-Commerce</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6you" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="text-white d-flex align-items-center gap-2">
+                    <FaCodeCompare size={30} />
+                    <p className="mb-0">
+                      Compare <br />
+                      Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="text-white d-flex align-items-center gap-2">
+                    <FaHeart size={30} />
+                    <p className="mb-0">
+                      Favorite <br />
+                      Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="text-white d-flex align-items-center gap-2">
+                    <FaRegUser size={30} />
+                    <p className="mb-0">
+                      Login <br />
+                      My Account
+                    </p>
+                  </Link>
+                </div>
 
-export default Header
+                <div>
+                  <Link className="text-white d-flex align-items-center gap-2">
+                    <FaCartFlatbed size={40} className="cart-icon" />
+                    <div className="d-flex flex-column gap-2">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
