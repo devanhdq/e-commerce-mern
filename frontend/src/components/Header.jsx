df787ac6324cc4fd52a4bb767d12b19b2608ffbc
devanhdq <dev.anhdq@gmail.com>
2024-07-08 23:41:59 +0700

    feat: Add dropdown menu to header component
    The code changes in the Header.jsx file add a dropdown menu to the header component. This menu allows users to access additional navigation options. The menu is implemented using the Bootstrap dropdown component.
    
    Recent user commits:
    - create component header
    - create frontend
    - feat: Add role field to user registration
    
    Recent repository commits:
    - create component header
    - chore: Update user.router.js and add ValidateMongodbId.js
    - create frontend
    - feat: Add role field to user registration

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import {
   FaCodeCompare,
@@ -99,7 +99,59 @@ const Header = () => {
       <div className="header-bottom py-3">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-12"></div>
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-15">
+                <div>
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      Dropdown button
+                    </button>
+                    <ul
+                      class="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Action
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Another action
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Something else here
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink className="text-white" to="/">
+                      Home
+                    </NavLink>
+                    <NavLink className="text-white" to="/store">
+                      Our Store
+                    </NavLink>
+                    <NavLink className="text-white" to="/blog">
+                      Blogs
+                    </NavLink>
+                    <NavLink className="text-white" to="/contact">
+                      Contact
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
