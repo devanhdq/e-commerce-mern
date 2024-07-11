aadc2b2c35a6b43ee6bcac3c02f4c19792c5c5ea
devanhdq <dev.anhdq@gmail.com>
2024-07-11 14:19:38 +0700

    Update font family in index.css to use "Rubik" instead of "Roboto"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 //
 import main_banner from "../assets/images/main-banner.jpg";
+import small_banner_1 from "../assets/images/catbanner-01.jpg";
+import small_banner_2 from "../assets/images/catbanner-02.jpg";
+import small_banner_3 from "../assets/images/catbanner-03.jpg";
+import small_banner_4 from "../assets/images/catbanner-04.jpg";
 
 const Home = () => {
   return (
@@ -10,44 +14,95 @@ const Home = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner-content position-relative p-3">
+              <div className="main-banner position-relative">
                 <img
                   src={main_banner}
-                  alt="banner"
+                  alt="Main banner"
                   className="img-fluid rounded-3"
                 />
                 <div className="main-banner-content position-absolute">
-                  <h4>Top Sale</h4>
-                  <h5>Ipad Air 6</h5>
-                  <p>From $699 or $41.2/mo</p>
+                  <h4>Significant others.</h4>
+                  <h5>iPhone and Mac</h5>
+                  <p>
+                    You can answer calls or messages from your iPhone directly
+                    on your Mac.
+                  </p>
                   <Link className="button">Buy Now</Link>
                 </div>
               </div>
             </div>
             <div className="col-6">
-          
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img
+                    src={small_banner_1}
+                    alt="Main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Significant others.</h4>
+                    <h5>iPhone and Mac</h5>
+                    <p>
+                      You can answer calls or messages
+                    </p>
+                    <Link className="small-button">Buy Now</Link>
+                  </div>
+                </div>
+
+                <div className="small-banner position-relative">
+                  <img
+                    src={small_banner_2}
+                    alt="Main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Significant others.</h4>
+                    <h5>iPhone and Mac</h5>
+                    <p>
+                      You can answer calls or messages
+                    </p>
+                    <Link className="small-button">Buy Now</Link>
+                  </div>
+                </div>
+
+                <div className="small-banner position-relative">
+                  <img
+                    src={small_banner_3}
+                    alt="Main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Significant others.</h4>
+                    <h5>iPhone and Mac</h5>
+                    <p>
+                      You can answer calls or messages
+                    </p>
+                    <Link className="small-button">Buy Now</Link>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src={small_banner_4}
+                    alt="Main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Significant others.</h4>
+                    <h5>iPhone and Mac</h5>
+                    <p>
+                      You can answer calls or messages
+                    </p>
+                    <Link className="small-button">Buy Now</Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
+      
     </>
   );
 };
 
 export default Home;
-
-{/* <div className="d-flex flex-wrap justify-content-between align-items-center">
-<div className="small-banner-content position-relative p-3">
-  <img
-    src={main_banner}
-    alt="banner"
-    className="img-fluid rounded-3"
-  />
-  <div className="small-banner-content position-absolute">
-    <h4>Top Sale</h4>
-    <h5>Ipad Air 6</h5>
-    <p>From $699 or $41.2/mo</p>
-    <Link className="button">Buy Now</Link>
-  </div>
-</div>
-</div> */}
